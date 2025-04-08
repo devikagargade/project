2854df7821efd89b4ac20eb79385fe6c3551c616
devikagargade <14devikagargade@gmail.com>
2025-04-08 17:23:23 +0530

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Hello, Streamlit! 👋")
-
-st.write("This is your first Streamlit app.")
-name = st.text_input("Enter your name:")
-if name:
-    st.success(f"Welcome, {name}!")
